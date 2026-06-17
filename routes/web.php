124c5186bc116e23028baa2a164e107d20db00a8
AJ <edulife1999@gmail.com>
2026-06-17 16:39:28 +0600

    changes

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,14 @@ Route::get('/', function () {
 });
 
 // Route::get('account/verify/{token}', [AuthController::class, 'verifyEmail'])->name('user.verify');
+
+
+Route::get('/server-check', function () {
+    return [
+        'php_version' => PHP_VERSION,
+        'curl' => extension_loaded('curl'),
+        'openssl' => extension_loaded('openssl'),
+        'allow_url_fopen' => ini_get('allow_url_fopen'),
+        'sapi' => php_sapi_name(),
+    ];
+});
