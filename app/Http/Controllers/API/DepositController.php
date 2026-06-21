cc15e44cb4996d68ebc45a8946000573b5808ae9
AJ <edulife1999@gmail.com>
2026-06-21 17:04:30 +0600

    changes

--- app/Http/Controllers/API/DepositController.php
+++ app/Http/Controllers/API/DepositController.php
@@ -124,7 +124,7 @@ class DepositController extends Controller
                     'amount'     => $depositJob->amount,
                     'wallet'     => $depositJob->wallet,
                     'status'     => $depositJob->status,
-                    'expires_at' => $depositJob->created_at->addMinutes(20)->timestamp,
+                    'expires_at' => $depositJob->created_at->addMinutes(1)->timestamp,
                 ]
             ]);
 
